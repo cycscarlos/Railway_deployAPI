6fd366f6da5c5ac51ebce076ee0b4eef7b3e821d
zCoder <cycs.ingenieria@gmail.com>
2025-01-31 18:09:04 -0400

    comentarios a los archivos de conexión de BDD

--- src/config.js
+++ src/config.js
@@ -1,10 +1,15 @@
+// Se utiliza tanto en desarrollo como en producción. Carga las variables de entorno desde .env en desarrollo y desde el entorno de producción en producción.
+
 import dotenv from "dotenv";
 
-dotenv.config(); // Cargar las variables de entorno desde el archivo .env
+dotenv.config(); // Carga las variables de entorno desde el archivo .env
 
 // verificamos que las variables de entorno se entá cargando correctamente
 console.log("config.js-> DB_USER:", process.env.DB_USER);
 console.log("config.js-> DB_PASSWORD:", process.env.DB_PASSWORD);
+
+// Railway
+// mysql://root:QXQUdQaYfyNcwCGtoyZCysstnRmxskon@autorack.proxy.rlwy.net:25004/railway
 
 // verificamos que nuestro proveedor en la nube nos este dando variables de entorno
 export const PORT = process.env.PORT || 4000;
--- src/db.js
+++ src/db.js
@@ -1,14 +1,16 @@
+// Se utiliza tanto en desarrollo como en producción. Establece la conexión a la base de datos utilizando las variables de entorno definidas en config.js.
+
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
-dotenv.config(); // Cargar las variables de entorno
+dotenv.config(); // Carga las variables de entorno desde el archivo .env
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT,
 });
 
 export { pool };
