e115545e86b8c43f3e1a2c6a8740fa25c42120af
zCoder <cycs.ingenieria@gmail.com>
2025-02-03 18:45:56 -0400

    ajustes al db.js

--- src/db.js
+++ src/db.js
@@ -3,14 +3,15 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
-dotenv.config(); // Carga las variables de entorno desde el archivo .env
+// Carga las variables de entorno desde el archivo .env
+dotenv.config(); 
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,
 });
 
 export { pool };
