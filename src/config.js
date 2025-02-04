9de52b8a642fe74ccfa432ff39a2d54a6682da4a
zCoder <cycs.ingenieria@gmail.com>
2025-02-04 10:29:05 -0400

    console.log en config.js para verificación de variables de entorno

--- src/config.js
+++ src/config.js
@@ -1,17 +1,24 @@
-// Se utiliza tanto en desarrollo como en producción. Carga las variables de entorno desde .env en desarrollo y desde el entorno de producción en producción.
+// Se utiliza tanto en desarrollo como en producción. Carga las variables de entorno desde .env en desarrollo o desde el proveedor de la nube, en un entorno de producción.
 
 import dotenv from "dotenv";
 
 dotenv.config(); // Carga las variables de entorno desde el archivo .env
 
-// verificamos que las variables de entorno se entá cargando correctamente desde el archivo .env
-// console.log("config.js-> DB_USER:", process.env.DB_USER);
-// console.log("config.js-> DB_PASSWORD:", process.env.DB_PASSWORD);
+// Ambiente de desarrollo. Verificación que las variables de entorno se estan cargando desde el archivo .env
+console.log("variable proveniente de .env | DB_HOST:", process.env.DB_HOST);
+console.log("variable proveniente de .env | DB_NAME:", process.env.DB_NAME);
+console.log("variable proveniente de .env | DB_USER:", process.env.DB_USER);
+console.log(
+  "variable proveniente de .env | DB_PASSWORD:",
+  process.env.DB_PASSWORD
+);
+console.log("variable proveniente de .env | DB_PORT:", process.env.DB_PORT);
 
-// verificamos que nuestro proveedor de la nube nos este dando el puerto como una variables de entorno
+// Sí el archivo .env o el proveedor de la nube NO proveé el puerto, entonces utiliza el 4000 por Default
+ 
 export const PORT = process.env.PORT || 4000;
 
-// verificamos que nuestro proveedor de la nube nos este dando las variables de entorno requeridas para establecer la conexión con la Base de Datos
+// Utiliza las variables de entorno del archivo .env o del proveedor de la nube para establecer la conexión con la Base de Datos
 export const DB_HOST = process.env.DB_HOST;
 export const DB_NAME = process.env.DB_NAME;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
